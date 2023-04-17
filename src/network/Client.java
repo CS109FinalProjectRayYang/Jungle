579235d5e51f102d716a87309f6ee299e642881d
Ray0v0 <947609598@qq.com>
2023-04-17 19:10:41 +0800

    给Client.java添加注释

--- src/network/Client.java
+++ src/network/Client.java
@@ -25,13 +25,14 @@ public class Client {
         JButton backInitButton = new JButton("返回主界面");
         //文本框
         //图片
-        ImageIcon jungleImg = new ImageIcon("data/img/Jungle.jpg");
+        ImageIcon mainFrameImg = new ImageIcon("data/img/Jungle.jpg");
         // source: https://www.zcool.com.cn/work/ZMzg3NjU3Njg=.html
         //标签
-        JLabel jungleImgLabel = new JLabel(jungleImg);
+        JLabel mainFrameImgLabel = new JLabel(mainFrameImg);
         //字体
         Font buttonFont = new Font("楷体", Font.BOLD, 20);
         public static void main(String[] args) {
+            // 设置Swing窗体风格
             try {
                 String lookAndFeel = "javax.swing.plaf.nimbus.NimbusLookAndFeel";
                 UIManager.setLookAndFeel(lookAndFeel);
@@ -40,37 +41,57 @@ public class Client {
                 throw new RuntimeException(e);
             }
 
+            // 新建GUI对象并运行
             GUI gui = new GUI();
             gui.run();
         }
         public void run() {
+            // 加载主窗体
             loadMainFrame();
+
+            // 加载初始界面的按钮面板
             loadInitButtonPanel();
+
+            // 加载本地游戏选择界面的按钮面板
             loadLocalGameButtonPanel();
-            jungleImg.setImage(jungleImg.getImage().getScaledInstance(700, 500, Image.SCALE_DEFAULT));
+
+            // 设置主窗体的图片大小
+            mainFrameImg.setImage(mainFrameImg.getImage().getScaledInstance(700, 500, Image.SCALE_DEFAULT));
+
+            // 在上述准备工作均完成后，绘制主窗体
             initPaint();
         }
         private void initPaint() {
+            // 清空主窗体
             mainFramePanel.removeAll();
-            mainFramePanel.add(jungleImgLabel);
+
+            // 在主窗体中加入主窗体图片以及初始界面的按钮面板
+            mainFramePanel.add(mainFrameImgLabel);
             mainFramePanel.add(initButtonPanel);
 
+            // 设置mainFrame大小以包装上述组件并显示
             mainFrame.pack();
             mainFrame.setVisible(true);
         }
         private void localGamePaint() {
+            // 清空主窗体
             mainFramePanel.removeAll();
-            mainFramePanel.add(jungleImgLabel);
+
+            // 在主窗体中加入主窗体图片以及本地游戏选择按钮面板
+            mainFramePanel.add(mainFrameImgLabel);
             mainFramePanel.add(localGameButtonPanel);
 
+            // 设置mainFrame大小以包装上述组件并显示
             mainFrame.pack();
         }
         private void onlineGamePaint() {
 
         }
         private void loadMainFrame() {
+            // 设置主窗体面板的布局
             mainFramePanel.setLayout(new BoxLayout(mainFramePanel, BoxLayout.X_AXIS));
 
+            // 设置主窗体基本属性，并将主窗体面板加入到主窗体上
             mainFrame = new JFrame();
             mainFrame.setContentPane(mainFramePanel);
             mainFrame.setLocation(300, 150);
@@ -78,6 +99,7 @@ public class Client {
 
         }
         private void loadInitButtonPanel() {
+            // 设置 本地游戏、在线游戏、退出游戏 这三个按钮的基本属性，并加入监听
             localGameButton.setFont(buttonFont);
             onlineGameButton.setFont(buttonFont);
             exitGameButton.setFont(buttonFont);
@@ -86,13 +108,15 @@ public class Client {
             onlineGameButton.addActionListener(new onlineGameButtonListener());
             exitGameButton.addActionListener(new exitGameButtonListener());
 
+            // 将三个按钮依次加入初始界面按钮面板中
             initButtonPanel.setLayout(new BoxLayout(initButtonPanel, BoxLayout.Y_AXIS));
             initButtonPanel.add(localGameButton);
             initButtonPanel.add(onlineGameButton);
-            initButtonPanel.add(Box.createVerticalGlue());
+            initButtonPanel.add(Box.createVerticalGlue()); // 退出游戏按钮上方做一定分隔
             initButtonPanel.add(exitGameButton);
         }
         private void loadLocalGameButtonPanel() {
+            // 设置 玩家对战、电脑对战、返回主界面 这三个按钮的基本属性，并加入监听
             againstPlayerButton.setFont(buttonFont);
             againstComputerButton.setFont(buttonFont);
             backInitButton.setFont(buttonFont);
@@ -101,6 +125,7 @@ public class Client {
             againstComputerButton.addActionListener(new againstComputerButtonListener());
             backInitButton.addActionListener(new backInitButtonListener());
 
+            // 将这三个按钮加入本地游戏按钮面板中
             localGameButtonPanel.setLayout(new BoxLayout(localGameButtonPanel, BoxLayout.Y_AXIS));
             localGameButtonPanel.add(againstPlayerButton);
             localGameButtonPanel.add(againstComputerButton);
